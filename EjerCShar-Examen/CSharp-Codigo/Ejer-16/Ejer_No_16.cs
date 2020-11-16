65884c4d34da525309a83381f351fe25bae2027a
Jose Rene Fuentes <jrfuent@gmail.com>
2020-11-16 03:45:27 +0100

    Ejercicio CSharp para el examen.

---
+++ EjerCShar-Examen/CSharp-Codigo/Ejer-16/Ejer_No_16.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Csharp_Code
+{
+    class Ejer_No_16
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor digite la URL con el formato https://www.dominio.com:");
+            string url = Console.ReadLine();
+            if (ExtensionMethods.IsUrl(url)) 
+                Console.WriteLine("Bien hecho, escribe muy bien");
+            else
+                Console.WriteLine("Lo siento no conoce la página de Microsoft, trate nuevamente!!");
+
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static bool IsUrl(this String str)
+        {
+            var regex = new Regex(
+                "(https?://)?([A-Za-z0-9-]*\\.)?([A-Za-z0-9-]*)" + 
+                "\\.[A-Za-z0-9]*/?.*");
+            return regex.IsMatch(str);
+        }
+    }
+}
---
+++ EjerCShar-Examen/CSharp-Codigo/Ejer-34/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Ejer_34
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("\n\n      Por favor digite 1, 2, o 3 para elegir una de la configuraciones siguientes:\n");
+            Console.WriteLine("              1. TRIAL.");
+            Console.WriteLine("              2. ADVANCED.");
+            Console.WriteLine("              3. BASIC");
+            Console.Write("\n              ");
+            string build = "\t" + Console.ReadLine();
+            if (build == "1")
+            {
+                Console.WriteLine("\n\n      *****************************************");
+                Console.WriteLine("          ESTAS EN LA CONFIGURACION TRIAL !");
+                Console.WriteLine("      *****************************************\n\n");
+                EvaluateLoan(); 
+            }
+            else if (build == "2")
+            {
+                Console.WriteLine("\n\n      *****************************************");
+                Console.WriteLine("         ESTAS EN LA CONFIGURACION ADVANCED !");
+                Console.WriteLine("      *****************************************\n\n");
+                EvaluateLoan();
+                ProcessLoan();
+                FundLoan();
+            }
+            else
+            {
+                Console.WriteLine("\n\n      *****************************************");
+                Console.WriteLine("           ESTAS EN LA CONFIGURACION BASIC !");
+                Console.WriteLine("      *****************************************\n\n");
+                EvaluateLoan();
+                ProcessLoan();
+            }
+
+            Console.WriteLine("\n\n              ");
+        }
+        static void EvaluateLoan()
+        {
+            Console.WriteLine("      Saludos desde el Método EvaluateLoan()!");
+        }
+        static void ProcessLoan()
+        {
+            Console.WriteLine("      Saludos desde el Método ProcessLoan()!");
+        }
+        static void FundLoan()
+        {
+            Console.WriteLine("      Saludos desde el Método FundLoan()!");
+        }
+    }
+}
