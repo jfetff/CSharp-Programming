962843a0d19558b5c01ed4dd7c3137273852b46a
Jose Rene Fuentes <jrfuent@gmail.com>
2020-11-29 00:22:51 +0100

    Ejercicio 272 ha sido agregado.

--- EjerCShar-Examen/CSharp-Codigo/Ejer-272/Program.cs
+++ EjerCShar-Examen/CSharp-Codigo/Ejer-272/Program.cs
@@ -4,9 +4,30 @@ namespace Ejer_272
 {
     class Program
     {
-        static void Main(string[] args)
+
+        // Declaración del delegate
+        public delegate int AddNumbers(int x, int y);
+
+        class TestDelegate
         {
-            Console.WriteLine("Hello World!");
+            // Creación de un metodo para el delegate.
+            public static int MyFunc(int x, int y)
+            {
+                Console.WriteLine("\n\n     He sido llamado por el delegate ...\n\n", string.Format("{0}", ((int)x + (int)y)));
+                return x + y;
+            }
+
+            public static void Main()
+            {
+                // Instanciación del delegate
+                AddNumbers sumar = new AddNumbers(MyFunc);
+                int a = 3;
+                int b = 7; 
+                string str = "";
+                // Invocación de la función delegate
+                str = Convert.ToString(sumar(a, b));
+                Console.WriteLine($"     La suma de los números {a} y {b} es: {str}");
+            }
         }
     }
 }
