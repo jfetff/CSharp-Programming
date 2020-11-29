10ef53dfc4a9fafac143499531245c022de999d9
Jose Rene Fuentes <jrfuent@gmail.com>
2020-11-29 02:02:48 +0100

    nuevos ejercicios se han agregados.

--- EjerCShar-Examen/CSharp-Codigo/Ejer-233/Program.cs
+++ EjerCShar-Examen/CSharp-Codigo/Ejer-233/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ejer_233
 {
@@ -6,7 +7,24 @@ namespace Ejer_233
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Version 1: create a Dictionary, then add 4 pairs to it.
+            var dictionary = new Dictionary<string, int>();
+            dictionary.Add("cat", 2);
+            dictionary.Add("dog", 1);
+            dictionary.Add("llama", 0);
+            dictionary.Add("iguana", -1);
+            // The dictionary has 4 pairs.
+            Console.WriteLine("\n\n     DICCIONARIO 1: " + dictionary.Count);
+            // Version 2: create a Dictionary with an initializer.
+            var dictionary2 = new Dictionary<string, int>()
+            {
+                {"cat", 2},
+                {"dog", 1},
+                {"llama", 0},
+                {"iguana", -1}
+            };
+            // This dictionary has 4 pairs too.
+            Console.WriteLine("\n     DICCIONARIO 2: " + dictionary2.Count);
         }
     }
 }
--- EjerCShar-Examen/CSharp-Codigo/Ejer-251/Program.cs
+++ EjerCShar-Examen/CSharp-Codigo/Ejer-251/Program.cs
@@ -9,4 +9,46 @@ namespace Ejer_251
             Console.WriteLine("Hello World!");
         }
     }
+
+    public class ContosoException : System.Exception
+    {
+        static void Log(Exception ex)
+        {
+            System.Console.WriteLine("    Este es el Log de ContosoException, con el mensaje: ", ex.Message);
+        }
+    }
+
+    public class ContosoDbException : Contoso.Exception
+    {
+        static void Log(ContosoException ex)
+        {
+            System.Console.WriteLine("    Este es el Log de ContosoDbException, con el mensaje: ", ex.Message);
+        }
+    }
+
+    public class ContosoValidationException : Contoso.Exception
+    {
+        static void Log(ContosoValidationException ex)
+        {
+            System.Console.WriteLine("    Este es el Log de ContosoValidationException, con el mensaje: ", ex.Message);
+        }
+    }
+
+
+    [Serializable]
+    class InvalidStudentNameException : Exception
+    {
+        public InvalidStudentNameException()
+        {
+
+        }
+
+        public InvalidStudentNameException(string name)
+            : base(String.Format("Invalid Student Name: {0}", name))
+        {
+
+        }
+    
+    }
+
 }
